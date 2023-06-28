68dce23858d55a35102ce39267ab0d654d22286c
tomato <tomatomato2121@gmail.com>
2023-06-29 04:33:39 +0900

    Separate the main page components.

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,30 @@
-import Image from 'next/image';
+import Image, {StaticImageData} from 'next/image';
 import Link from "next/link";
 import ImagePath from '@/public/main-page-img/insung-yoon-MlY6reLHP8w-unsplash.jpg';
+
+interface ImageLinkProps {
+    href: string;
+    src: StaticImageData;
+    alt: string;
+}
+
+function ImageLink({href, src, alt}: ImageLinkProps) {
+    return (
+        <Link href={href} className="flex-1">
+            <Image
+                src={src}
+                alt={alt}
+                className="object-cover w-full h-full" />
+        </Link>
+    )
+}
 
 export default function Home() {
     return (
         <main className="h-screen flex flex-row flex-1">
-            <Link href={`/`} className="flex-1">
-                <Image src={ImagePath} alt={`한국화`} className="object-cover w-full h-full" />
-            </Link>
-            <Link href={`/`} className="flex-1">
-                <Image src={ImagePath} alt={`서양화`} className="object-cover w-full h-full" />
-            </Link>
-            <Link href={`/`} className="flex-1">
-                <Image src={ImagePath} alt={`박물관`} className="object-cover w-full h-full" />
-            </Link>
+            <ImageLink href={`/`} src={ImagePath} alt={`서양화`} />
+            <ImageLink href={`/`} src={ImagePath} alt={`한국화`} />
+            <ImageLink href={`/`} src={ImagePath} alt={`박물관`} />
         </main>
     )
 }
