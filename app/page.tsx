565c057fa4b251a184d1212324c71a1a03a40995
tomato <tomatomato2121@gmail.com>
2023-06-29 04:15:33 +0900

    set up the layout for the main page image

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Link from "next/link";
+import ImagePath from '@/public/main-page-img/insung-yoon-MlY6reLHP8w-unsplash.jpg';
 
 export default function Home() {
     return (
         <main className="h-screen flex flex-row flex-1">
-            <div className="flex-1 bg-blue-200">
-                a
-            </div>
-            <div className="flex-1 bg-yellow-200">
-                a
-            </div>
-            <div className="flex-1 bg-red-200">
-                a
-            </div>
+            <Link href={`/`} className="flex-1">
+                <Image src={ImagePath} alt={`한국화`} className="object-cover w-full h-full" />
+            </Link>
+            <Link href={`/`} className="flex-1">
+                <Image src={ImagePath} alt={`서양화`} className="object-cover w-full h-full" />
+            </Link>
+            <Link href={`/`} className="flex-1">
+                <Image src={ImagePath} alt={`박물관`} className="object-cover w-full h-full" />
+            </Link>
         </main>
     )
 }
