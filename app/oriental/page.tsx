ebaaf9707c671693bfdba7bd12a5c32b8eb52bfb
PRESSO <caff1nepill@gmail.com>
2023-06-30 00:41:06 +0900

    image grid

--- app/oriental/page.tsx
+++ app/oriental/page.tsx
@@ -6,25 +6,29 @@ const prisma = new PrismaClient();
 const Page = async () => {
     const data = await prisma.artwork.findMany({
         where: {
-            type: "한국화"
+            type: "한국화",
+            image: {
+                not: {
+                    contains: "art_default2.gif"
+                }
+            }
         },
-        take: 10,
+        take: 100,
     });
 
     return (
-        <div>
-            {
-                data.map(async (item) => {
-                    return (
-                        <div key={item.title} className={`px-4 py-2 border-b`}>
-                            <div>{item.title}</div>
-                            <div>{item.description}</div>
-                            <div>{item.url}</div>
-                            <Image src={`http://artbank.go.kr${item.image}`} alt={item.image} width={500} height={500} />
-                        </div>
-                    )
-                })
-            }
+        <div className={`px-10 py-8`}>
+            <div className={`columns-5`}>
+                {
+                    data.map(async (item) => {
+                        return (
+                            <div key={item.title} className={`inline-block mb-2`}>
+                                <Image src={`http://artbank.go.kr${item.image}`} alt={item.image} width={500} height={500} />
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 };
