a4af7b45fca7d7db33a5e09bac938d5abb2b389a
PRESSO <caff1nepill@gmail.com>
2023-07-02 13:29:55 +0900

    add western page

--- app/western/page.tsx
+++ app/western/page.tsx
@@ -1,7 +1,45 @@
-const Page = () => {
+import { prisma } from "@/utils/prisma";
+import dynamic from "next/dynamic";
+import ArtworkGrid from "@/app/components/ArtworkGrid";
+const Pagination = dynamic(() => import("@/app/components/Pagination"), { ssr: false });
+
+interface Params {
+    searchParams: { [key: string]: string | string[] | undefined }
+}
+
+const Page = async ({ searchParams }: Params) => {
+    const page = (searchParams.page && (!isNaN(Number(searchParams.page)) && Number(searchParams.page) > 0)) ? Number(searchParams.page) : 1;
+
+    let data = await prisma.artwork.findMany({
+        where: {
+            type: "서양화",
+            image: {
+                not: {
+                    contains: "art_default2.gif"
+                }
+            }
+        },
+        // 범위
+        skip: (page - 1) * 50,
+        take: 50,
+    });
+
+    const count = await prisma.artwork.count({
+        where: {
+            type: "서양화",
+            image: {
+                not: {
+                    contains: "art_default2.gif"
+                }
+            }
+        }
+    });
+
     return (
-        <div>
-            western
+        <div className={`px-10 py-8`}>
+            {/* @ts-ignore */}
+            <ArtworkGrid data={data} />
+            <Pagination type={"western"} count={count} nowPage={page} />
         </div>
     );
 };
