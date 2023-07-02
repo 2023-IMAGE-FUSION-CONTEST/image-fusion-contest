35609d9995392c9577a2bd249efbceecff058c5d
tomato <tomatomato2121@gmail.com>
2023-07-02 19:04:48 +0900

    Modify the API endpoint.

--- app/api/route.ts
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-import {prisma} from "@/app/lib/prisma";
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-    const users = await prisma.artwork.findMany();
-    return NextResponse.json(users);
-}
