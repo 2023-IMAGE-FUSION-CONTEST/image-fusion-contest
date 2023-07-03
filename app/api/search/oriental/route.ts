0f59cf925c5b4407f70e141f5b0f2165886bfabf
tomato <tomatomato2121@gmail.com>
2023-07-04 02:24:25 +0900

    Successful GET API test.

--- app/api/search/oriental/route.ts
+++ app/api/search/oriental/route.ts
@@ -2,14 +2,17 @@ import {prisma} from "@/utils/prisma";
 import { NextResponse } from "next/server";
 
 
-export async function GET( request: Request, { params }: { params: { title: string } } ) {
-    const title: string = params.title;
+export async function GET( request: Request ) {
     const url: URL = new URL(request.url);
     const q: string | null = url.searchParams.get("q");
-    console.log(q);
     const user = await prisma.artwork.findMany({
         where: {
-            type: "서양화",
+            type: "한국화",
+            image: {
+                not: {
+                    contains: "art_default2.gif"
+                }
+            }
         },
     });
 
--- app/gallery/oriental/page.tsx
+++ app/gallery/oriental/page.tsx
@@ -7,22 +7,19 @@ interface Params {
     searchParams: { [key: string]: string | string[] | undefined }
 }
 
+async function getImage(queryString: string | string[] | undefined) {
+    const result = await fetch(
+      `http://localhost:3000/api/search/oriental?q=${queryString}`
+    );
+    const imageData = await result.json();
+    return imageData;
+}
+
+
 const Page = async ({ searchParams }: Params) => {
     const page = (searchParams.page && (!isNaN(Number(searchParams.page)) && Number(searchParams.page) > 0)) ? Number(searchParams.page) : 1;
 
-    let data = await prisma.artwork.findMany({
-        where: {
-            type: "한국화",
-            image: {
-                not: {
-                    contains: "art_default2.gif"
-                }
-            }
-        },
-        // 범위
-        skip: (page - 1) * 50,
-        take: 50,
-    });
+    let data = await getImage(searchParams.q);
 
     const count = await prisma.artwork.count({
         where: {
