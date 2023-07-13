1279e58db98b62a70ead1449ea53df6365616c16
tomato <tomatomato2121@gmail.com>
2023-07-13 21:30:46 +0900

    Remember previous chat

--- app/components/chat/input/Input.tsx
+++ app/components/chat/input/Input.tsx
@@ -3,14 +3,23 @@
 import { useState } from "react";
 import { useChatList } from "@/app/store/state";
 import Button from "@/app/components/chat/input/Button";
+import {responseParse} from "@/utils/sessionStorage";
 
 const Input = () => {
     const [loading, setLoading] = useState(false);
     const [input, setInput] = useState("");
     const setChatList = useChatList(state => state.setList);
 
-    const prompt = `Q: ${input}Generate a response with less than 100 characters.`
-
+    const prompt = `Q: ${input}
+        이전 질문, 답변: ${sessionStorage.getItem("before")}
+        해당 질문의 [ ] 속에는 이전 대화 기록이 들어있어, 만약 비어있다면 우린 처음 대화하는거야,
+        이전 대화를 토대로 현재 질문에 대해 200글자 이내의 답변을 제공해줘, 답변을 제공한 후 "<>==<>" 를 적은 후 해당 질문과 답변의 요약을 Q: 질문 A: 답변 형식으로 요약해줘
+        해당 질문에 대한 답변만 제공하면 되므로, 이전 질문에 대한 설명은 답변 안해도 돼
+        
+        예를들어 "Q: 리그오브레전드에 대해 아나요?.... A: 리그오브레전드는 이러한 것입니다... <>===<> Q: 리그오브레전드에 대해 질문 A: 리그오브 레전드에 대한 설명 요약"
+        
+        이러한 형식이 되야해
+    `
     const generateResponse = (e: any) => {
         e.preventDefault();
         setLoading(true);
@@ -39,7 +48,7 @@ const Input = () => {
                     const chunkValue = decoder.decode(value);
                     response += chunkValue;
                 }
-
+                response = responseParse(response);
                 setChatList(response);
                 setLoading(false);
             });
