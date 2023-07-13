a09f1c3896e202d03326480afd2fd32d49bf1a2a
tomato <tomatomato2121@gmail.com>
2023-07-13 22:26:53 +0900

    Write a prompt

--- app/components/chat/input/Input.tsx
+++ app/components/chat/input/Input.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from "react";
-import { useChatList } from "@/app/store/state";
+import { useChatList, useImageDetail } from "@/app/store/state";
 import Button from "@/app/components/chat/input/Button";
 import {responseParse} from "@/utils/sessionStorage";
 
@@ -9,17 +9,29 @@ const Input = () => {
     const [loading, setLoading] = useState(false);
     const [input, setInput] = useState("");
     const setChatList = useChatList(state => state.setList);
+    const imageDetail = useImageDetail(state => state.description);
+    const imageAuthor = useImageDetail(state => state.author);
 
-    const prompt = `Q: ${input}
-        이전 질문, 답변: ${sessionStorage.getItem("before")}
-        해당 질문의 [ ] 속에는 이전 대화 기록이 들어있어, 만약 비어있다면 우린 처음 대화하는거야,
-        이전 대화를 토대로 현재 질문에 대해 200글자 이내의 답변을 제공해줘, 답변을 제공한 후 "<>==<>" 를 적은 후 해당 질문과 답변의 요약을 Q: 질문 A: 답변 형식으로 요약해줘
-        해당 질문에 대한 답변만 제공하면 되므로, 이전 질문에 대한 설명은 답변 안해도 돼
+    const prompt = `
+    Your role as an AI is to provide answers to questions related to an image that I provide.
+    Sentences that start with "D:" are descriptions of the image, while sentences that start with "Q:" are user's questions.
+    Sentences that start with "R:" are records of the previous chats, formatted as [Q: Question A: Answer]. 
+    These reference previous conversations, and if it's empty, it implies that it's the first conversation.
+
+        D: ${imageDetail}, author: ${imageAuthor}
+        Q: ${input}
+        R: ${sessionStorage.getItem("before")}
         
-        예를들어 "Q: 리그오브레전드에 대해 아나요?.... A: 리그오브레전드는 이러한 것입니다... <>===<> Q: 리그오브레전드에 대해 질문 A: 리그오브 레전드에 대한 설명 요약"
-        
-        이러한 형식이 되야해
+    Your goal is to provide answers to the user's current questions related to the image, based on the previous chat. If you know additional information that's not included in the description, explain it.
+    After providing an answer, mark with "<>==<>" and summarize the question and answer in the format "Q: Question A: Answer".
+
+    For example, for a question like "D: Description of the image... Q: Question about the image... A: Answer about the image...", the response should be something like "My thoughts on the given image and question are...".
+    Avoid repeating sentences starting with "D:", "Q:", "R:".
+
+Responses should be provided in Korean.
     `
+
+
     const generateResponse = (e: any) => {
         e.preventDefault();
         setLoading(true);
